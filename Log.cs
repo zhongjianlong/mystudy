3bb71c61a75109a68cdfae384930af2c4f82ba86
钟件龙 <1538527036@qq.com>
2020-04-20 15:18:47 +0800

    NlogMode

---
+++ Log.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog
+{
+    public sealed class Log
+    {
+        private static NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+
+        private Log() { }
+
+        public static void Trace(string strMsg)
+        {
+            _logger.Trace(strMsg);
+        }
+         
+        public static void Debug(string strMsg)
+        {
+            _logger.Debug(strMsg);
+        }
+
+        public static void Info(string strMsg)
+        {
+            _logger.Info(strMsg);
+        }
+
+        public static void Warn(string strMsg)
+        {
+            _logger.Warn(strMsg);
+        }
+
+        public static void Warn(Exception e)
+        {
+            _logger.Warn(e);
+        }
+
+        public static void Warn(Exception e,string strMsg)
+        {
+            _logger.Warn(e,strMsg);
+        }
+         
+        public static void Error(string strMsg)
+        {
+            _logger.Error(strMsg);
+        }
+
+        public static void Error(Exception e, string strMsg)
+        {
+            _logger.Error(strMsg);
+        }
+
+        public static void Error(Exception e)
+        {
+            _logger.Error(e);
+        }
+
+        public static void Fatal(string strMsg)
+        {
+            _logger.Fatal(strMsg);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("开始 ...");
+
+            try
+            {
+                throw new Exception("测试错误");
+            }
+            catch (Exception e)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Log.Error(e.Message);
+
+                    Console.WriteLine($"执行了{i + 1}条记录 ...");
+                }
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
